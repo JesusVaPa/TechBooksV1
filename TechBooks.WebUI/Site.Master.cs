ebbd08dff77376969b510ce5b788247ce86d9c08
JesusVaPa <vapa.jesusangel@gmail.com>
2024-09-12 13:34:54 -0400

    WebUI and solution implemented

---
+++ TechBooks.WebUI/Site.Master.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Web.Security;
+using System.Web.UI;
+
+namespace TechBooks.WebUI
+{
+    public partial class Site : System.Web.UI.MasterPage
+    {
+        public string DangerMessage
+        {
+            set
+            {
+                lblErrorMessage.Text = value;
+                pnlDangerMessage.Visible = !string.IsNullOrEmpty(value);
+            }
+        }
+
+        public string SuccessMessage
+        {
+            set
+            {
+                lblSuccessMessage.Text = value;
+                pnlSuccessMessage.Visible = !string.IsNullOrEmpty(value);
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.Title == "Home")
+                lblTitle.Visible = false;
+            else
+                lblTitle.Text = Page.Title;
+
+            if (Page.User.Identity.IsAuthenticated)
+            {
+                pnlSignedIn.Visible = true;
+                pnlSignedOut.Visible = false;
+            }
+            else
+            {
+                pnlSignedIn.Visible = false;
+                pnlSignedOut.Visible = true;
+            }
+
+            DangerMessage = "";
+            SuccessMessage = "";
+
+        }
+
+        protected void lkbSignOut_Click(object sender, EventArgs e)
+        {
+            FormsAuthentication.SignOut();
+            Response.Redirect("/");
+        }
+    }
+}
